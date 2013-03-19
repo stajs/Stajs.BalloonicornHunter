f395a999ef0629c736f7cdc072bd0b2d4b24ad17
Shannon Skinner <shannonsnz@gmail.com>
2013-03-19 23:37:24 +1300

    Added tests for UTF combined characters

---
+++ Stajs.BalloonicornHunter.Core.Tests/Extensions/ArrayExtensionsTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+using Stajs.BalloonicornHunter.Core.Extensions;
+
+namespace Stajs.BalloonicornHunter.Core.Tests.Extensions
+{
+	[TestClass]
+	public class ArrayExtensionsTests
+	{
+		[TestMethod]
+		public void HandleUtfCombinedCharacters()
+		{
+			const string base64PlayerNameWithCombinedCharacters = "W1N5bGFyXeKEog==";
+			const string expected = "[Sylar]™";
+
+			var bytes = Convert.FromBase64String(base64PlayerNameWithCombinedCharacters);
+			
+			int length;
+			var actual = bytes.ReadStringUntilNullTerminator(out length);
+
+			actual.Should().Be(expected);
+			length.Should().BeGreaterThan(actual.Length);
+		}
+	}
+}
---
+++ Stajs.BalloonicornHunter.Core.Tests/Server/PlayerResponseTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Stajs.BalloonicornHunter.Core.Server;
+
+namespace Stajs.BalloonicornHunter.Core.Tests.Server
+{
+	[TestClass]
+	public class PlayerResponseTests
+	{
+		[TestMethod]
+		public void HandleUtfPlayerName()
+		{
+			const string base64PlayerResponseWithUtfCombinedCharactersInPlayerName = "/////0QWAHM4LiBrcnV6AI8AAAA4Y2hGAFNhdXNhZ2UgaW4gYSBCb3gACwAAANwpBUYASm95ZnVsU29ycm93WgAaAAAAoib9RQBKaW1teQCfAAAAjz/NRQBNeXN0ZXJpYWwgQj5mYW0gZmV6L2Jvb3RpZSB0aW1lABwAAADEBshFAFRvbSBIYW5rcwBQAAAAZ9q9RQBELUotTS05OQAmAAAAU2WtRQBCT0RZU0hPVEFJTUJPVC5FWEUARgAAAEkGmEUATWVyY2VuYXJ5AC4AAAAzpJJFAFBBSU5JUyBDVVBDQUtFAAAAAACaO4JFAHM4LiBOb20gSmVyZW15ABUAAAAzd3tFAEVkd2FyZCBSeWtmaWVsZAAIAAAAmo9xRQBMZU1vdGNoAAgAAACx1WtFAEJsYWNrIENhdmlhcgAXAAAAAlhpRQBbU3lsYXJd4oSiACAAAABd61dFAHBvbnl8ZC1fLWLimavimarimavimavimarimarimaoABAAAAIJP4kQAU2FsbW9uIDIxNDIAAwAAALiGiEQAVGVuZGVybG9pbnMABAAAAHX8dkQAeERzOkhpVEdpUkwgxrjMtcyh05zMtcyozITGtwADAAAAOlpeRADilqAgRG91Y2hlIEJpZ2Fsb3cg4pagAAMAAAB/xMJDAE1JTE9NT05TVEVSNQADAAAAayKSQwBDYWxkYXdn4oSiAAAAAADArRdC";
+			const string playerNameWithUtfCombinedCharacters = "[Sylar]™";
+			var bytes = Convert.FromBase64String(base64PlayerResponseWithUtfCombinedCharactersInPlayerName);
+
+			var playerResponse = new PlayerResponse(bytes);
+
+			playerResponse.Players.Should().ContainSingle(p => p.Name == playerNameWithUtfCombinedCharacters);
+		}
+	}
+}
