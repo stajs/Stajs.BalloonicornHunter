b9c92362e0e6b853aab49fbadfca47eb554419e6
Shannon Skinner <shannonsnz@gmail.com>
2013-03-28 22:32:40 +1300

    Find links

---
+++ Stajs.BalloonicornHunter.Core.Tests/SteamIdFinderTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using CsQuery;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Stajs.BalloonicornHunter.Core.Tests
+{
+	[TestClass]
+	public class SteamIdFinderTests
+	{
+		private CQ GetMultiUserDom()
+		{
+			var bytes = Convert.FromBase64String(ExampleHtml.MultipleResults);
+			var s = Encoding.UTF8.GetString(bytes);
+			return CQ.CreateDocument(s);
+		}
+
+		[TestMethod]
+		public void FindLinks_MultipleUsers_Returns50()
+		{
+			var steamIdFinder = new SteamIdFinder();
+			var links = steamIdFinder.FindUrls(GetMultiUserDom());
+
+			links.Should().HaveCount(50);
+			Assert.IsTrue(links.All(a => a.StartsWith("http://steamcommunity.com/profiles/") ||  a.StartsWith("http://steamcommunity.com/id/")));
+		}
+	}
+}
---
+++ Stajs.BalloonicornHunter.Core/SteamIdFinder.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CsQuery;
+
+namespace Stajs.BalloonicornHunter.Core
+{
+	public class SteamIdFinder
+	{
+		public Int64? Get(string name)
+		{
+			return Get(CQ.CreateFragment(""));
+		}
+
+		public Int64? Get(CQ dom)
+		{
+			return null;
+		}
+
+		internal List<string> FindUrls(CQ dom)
+		{
+			return dom["div.resultItem a.linkTitle"]
+				.Select(a => a["href"])
+				.ToList();
+		}
+	}
+}
